2fdc30a683e4722e157a3e9d2ae2778a6919d137
Eugene Nedilko <enedilko@oggettoweb.com>
2021-11-04 18:47:12 +0300

    Add meetup info

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,51 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      default: '',
+    },
+
+    place: {
+      type: String,
+      default: '',
+    },
+
+    date: {
+      type: String,
+      default: '',
+    }
+  },
+
+  computed: {
+    formattedDate() {
+      const date = new Date(this.date);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+
+      return date.toLocaleDateString('en-US', options);
+    },
+
+    formattedDateTime() {
+      const date = new Date(this.date);
+
+      return date.toISOString().split('T')[0];
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formattedDateTime">{{ formattedDate }}</time>
       </li>
     </ul>`,
 });
