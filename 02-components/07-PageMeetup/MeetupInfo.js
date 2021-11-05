6fa5ed143f7eea195be33b72d8c6a057e73f3e66
Eugene Nedilko <enedilko@oggettoweb.com>
2021-11-05 11:16:57 +0300

    Combine components for making PageMeetup

---
+++ 02-components/07-PageMeetup/MeetupInfo.js
@@ -0,0 +1,53 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupInfo',
+
+  props: {
+    organizer: {
+      type: String,
+      default: '',
+    },
+
+    place: {
+      type: String,
+      default: '',
+    },
+
+    date: {
+      type: Number,
+      default: '',
+    }
+  },
+
+  computed: {
+    formattedDate() {
+      const date = new Date(this.date);
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+
+      return date.toLocaleDateString('en-US', options);
+    },
+
+    formattedDateTime() {
+      const date = new Date(this.date);
+
+      return date.toISOString().split('T')[0];
+    },
+  },
+
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{ organizer }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{ place }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time :datetime="formattedDateTime">{{ formattedDate }}</time>
+      </li>
+    </ul>`,
+});
